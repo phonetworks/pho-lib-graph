534c18fdea385e6216dbc72091f977073e4668d3
Groups <esokullu@gmail.com>
2017-04-24 21:27:27 +0300

    rearranged folders of unit tests

--- tests/Pho/Lib/Graph/SimpleTest.php
+++ tests/Pho/Lib/Graph/SimpleTest.php
@@ -9,14 +9,14 @@
  * file that was distributed with this source code.
  */
 
-use Pho\Lib\Graph;
+namespace Pho\Lib\Graph;
 
 class SimpleTest extends \PHPUnit\Framework\TestCase 
 {
     private $graph;
 
     public function setUp() {
-        $this->graph = new Graph\Graph();
+        $this->graph = new Graph();
     }
 
     public function tearDown() {
@@ -24,33 +24,33 @@ class SimpleTest extends \PHPUnit\Framework\TestCase
     }
 
     public function testGraphAddGet() {
-        $node = new Graph\Node($this->graph);
+        $node = new Node($this->graph);
         $node_expected_to_be_identical = $this->graph->get($node->id());
         $this->assertEquals($node->id(), $node_expected_to_be_identical->id());
     }
 
     public function testGraphContains() {
-        $node =new Graph\Node($this->graph);
+        $node =new Node($this->graph);
         $this->assertTrue($this->graph->contains($node->id()));
     }
 
     public function testSubgraph() {
-        $subgraph = new Graph\SubGraph($this->graph);
+        $subgraph = new SubGraph($this->graph);
         $this->assertTrue($this->graph->contains($subgraph->id()));
     }
 
     public function testSubgraphRecursiveness() {
-        $subgraph = new Graph\SubGraph($this->graph);
-        $node = new Graph\Node($subgraph);
+        $subgraph = new SubGraph($this->graph);
+        $node = new Node($subgraph);
         $this->assertTrue($subgraph->contains($node->id()));
         $this->assertTrue($this->graph->contains($node->id()));
         $this->assertTrue($this->graph->contains($subgraph->id()));
     }
 
     public function testEdge() {
-        $node1 = new Graph\Node($this->graph);
-        $node2 = new Graph\Node($this->graph);
-        $edge = new Graph\Edge($node1, $node2);
+        $node1 = new Node($this->graph);
+        $node2 = new Node($this->graph);
+        $edge = new Edge($node1, $node2);
         $this->assertEquals($edge->id(), $node1->edges()->out()[0]->id());
         $this->assertEquals($edge->id(), $node2->edges()->in()[0]->id());
         $this->assertEquals($edge->id(), $node2->edges()->all()[0]->id());
@@ -58,9 +58,9 @@ class SimpleTest extends \PHPUnit\Framework\TestCase
 
     public function testAttributes() {
         $faker = \Faker\Factory::create();
-        $node1 = new Graph\Node($this->graph);
-        $node2 = new Graph\Node($this->graph);
-        $edge = new Graph\Edge($node1, $node2);
+        $node1 = new Node($this->graph);
+        $node2 = new Node($this->graph);
+        $edge = new Edge($node1, $node2);
         $node1->attributes()->username = ($username1 = $faker->username);
         $node2->attributes()->username = ($username2 = $faker->username);
         $edge->attributes()->address = ($address = $faker->address);
@@ -70,18 +70,18 @@ class SimpleTest extends \PHPUnit\Framework\TestCase
     }
 
     public function testPredicateAssignment() {
-        $new_predicate = new class extends Graph\Predicate { public function test() { return "works"; }};
-        $node1 = new Graph\Node($this->graph);
-        $node2 = new Graph\Node($this->graph);
-        $edge1 = new Graph\Edge($node1, $node2);
+        $new_predicate = new class extends Predicate { public function test() { return "works"; }};
+        $node1 = new Node($this->graph);
+        $node2 = new Node($this->graph);
+        $edge1 = new Edge($node1, $node2);
         $this->assertFalse(method_exists($edge1->predicate(), "test"));
-        $edge2 = new Graph\Edge($node1, $node2, $new_predicate);
+        $edge2 = new Edge($node1, $node2, $new_predicate);
         $this->assertEquals("works", $edge2->predicate()->test());
     }
 
     public function testID() {
-        $id1 = Graph\ID::generate();
-        $id2 = Graph\ID::fromString((string)$id1);
+        $id1 = ID::generate();
+        $id2 = ID::fromString((string)$id1);
         $this->assertEquals($id1, $id2);
     }
 
@@ -89,6 +89,6 @@ class SimpleTest extends \PHPUnit\Framework\TestCase
      * @expectedException  \Pho\Lib\Graph\Exceptions\MalformedGraphIDException
      */
     public function testInvalidID() {
-        Graph\ID::fromString("invalid");
+        ID::fromString("invalid");
     }
 }
